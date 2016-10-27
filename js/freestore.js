1b77dec83eb84f630610c9926269501a77c50a4d
bitmold <bitmold@gmail.com>
2016-10-27 13:05:25 -0400

    forgot to commit

--- js/freestore.js
+++ js/freestore.js
@@ -2,7 +2,6 @@
 	var done = false;
 	var index = 0;
 	var items = [65, 78, 65, 82, 67, 72, 89];
-	var texts = ['"The ultimate end of all revolutionary social change is to establish the sanctity of human life, the dignity of man, the right of every human being to liberty and well-being." ~ Emma Goldman'];
 	window.onkeyup = function(e) {
 	   if (done) return;
 	   var key = e.keyCode ? e.keyCode : e.which;
@@ -16,9 +15,14 @@
 	   			document.title = "Ⓐ❤️woo free store❤️️Ⓐ"
 	   			var marquee = $("<marquee>");
 	   			marquee.attr("behavior", "alternating");
-	   			marquee.text(texts[Math.floor(texts.length * Math.random())]);
+	   			marquee.text(getQuote());	
 	   			$($('.row')[0]).prepend(marquee);
 	   		}
 	   }
 	   else index = 0;
 }})();
+
+function getQuote() {
+	var texts = ['"The ultimate end of all revolutionary social change is to establish the sanctity of human life, the dignity of man, the right of every human being to liberty and well-being." ~ Emma Goldman'];
+	return texts[Math.floor(Math.random() * texts.length)];
+} 
